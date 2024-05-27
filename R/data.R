adb000138ba11c00b4a2fabc4062bd09226487c8
Samantha Malatesta <samalate@bu.edu>
2024-05-26 22:27:00 -0400

    continue set up

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' TBebmData
+#'
+#' A simulated data set to fit the EBM. The data contains 3 clinical measures with 2,3,and 1 events respectively for a total of 6 disease events.
+#'
+#' @format
+#' A data frame with 250 rows and 8 columns:
+#' \describe{
+#'   \item{Index}{unique row identifier}
+#'   \item{stage}{disease stage}
+#'   \item{V1}{Clinical measure 1, level 1}
+#'   \item{V2}{Clinical measure 1, level 2}
+#'   \item{V3}{Clinical measure 2, level 1}
+#'   \item{V4}{Clinical measure 2, level 2}
+#'   \item{V5}{Clinical measure 2, level 3}
+#'   \item{V6}{Clinical measure 3, level 1}
+#' }
+"TBebmData"
