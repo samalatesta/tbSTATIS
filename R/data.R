cd69073c6657f45db1ad61629fbcb47dda2c7f81
samalatesta <samalate@bu.edu>
2025-03-21 16:37:52 -0400

    Update example data set.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' TBData
 #'
-#' A simulated data set to fit TB-STATIS. The data contains 3 clinical measures with 2,3,and 1 events respectively for a total of 6 disease events.
+#' A simulated data set to fit TB-STATIS. The data contains 4 clinical measures.
 #'
 #' @format
 #' A data frame with 250 rows and 8 columns:
@@ -8,11 +8,11 @@
 #'   \item{Index}{unique row identifier}
 #'   \item{class}{disease class}
 #'   \item{X1}{Clinical measure 1, state 1}
-#'   \item{X2}{Clinical measure 1, state 2}
-#'   \item{X3}{Clinical measure 2, state 1}
-#'   \item{X4}{Clinical measure 2, state 2}
-#'   \item{X5}{Clinical measure 2, state 3}
-#'   \item{X6}{Clinical measure 3, state 1}
+#'   \item{X2}{Clinical measure 2, state 1}
+#'   \item{X3}{Clinical measure 2, state 2}
+#'   \item{X4}{Clinical measure 3, state 1}
+#'   \item{X5}{Clinical measure 3, state 2}
+#'   \item{X6}{Clinical measure 4, state 1}
 #'   \item{C1}{Covariate, categorical}
 #'   \item{C2}{Covariate, continuous}
 #' }
