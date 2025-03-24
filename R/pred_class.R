56b6e7fb12e3e428400a2cdcf98c8551ad8ef16d
samalatesta <samalate@bu.edu>
2025-03-24 13:43:53 -0400

    Update pred class code and doc.

--- R/pred_class.R
+++ R/pred_class.R
@@ -1,35 +1,30 @@
-#' Get disease class
-#' @param data A data frame.
-#' @param S Data frame
-#' @param p A vector
+#' Get predicted disease class
+#' @description
+#' Get predicted disease class for each individual in sample given their observed data and an estimated
+#' disease sequence from fitting TB-STATIS.
+#'
+#' @param data A data frame of observed data.
+#' @param S A data frame for the estimated disease sequence.
+#' @param p A vector of clinical state accuracies.
 #' @return A data frame.
 
 pred_class <- function(data=data.frame(), S=data.frame(), p=vector()){
 
-#data=dplyr::arrange(data,Index)
-likelihood=get_likelihood(data, S, p )
+    #get likelhood
+    likelihood=get_likelihood(data, S, p )
 
-class_probs <- likelihood[[2]]
+    #save class probs
+    stage_probs <- likelihood[[2]]
+    stage_probs=data.frame(stage_probs)
+    #update colnames to class numbers
+    colnames(stage_probs) = c(0:max(S$sub))
 
+    #assign disease class with max prob
+    pred_stage=as.numeric(colnames(stage_probs)[apply(stage_probs,1,which.max)])
 
-class_probs=data.frame(class_probs)
-colnames(class_probs) = c(0:max(S$sub))
+    #cbind to original data
+    classdf <- cbind(data,pred_stage)
 
-pred_class=as.numeric(colnames(class_probs)[apply(class_probs,1,which.max)])
+    return(classdf)
+  }
 
-classdf <- cbind(data,pred_class)
-
-return(classdf)
-}
-
-
-#S=new_order
-#measure_info <- data.frame(clinical_measure=c("measure1", "measure1", "measure2", "measure2", "measure2", "measure3"),event_number = c(1,2,1,2,3,1), event_name=c(c("X1", "X2", "X3", "X4", "X5", "X6")))
-
-#data <- TBebmData[,c(1,3:8)]
-#S=TBebm::get_seq(measure_info)
-#poss=TBebm::possible_seqs(6)
-#S <- TBebm::get_group(S, poss)
-#p<- c(.8,.8,.8, .8, .8,.8)
-
-#head(stagedf)
