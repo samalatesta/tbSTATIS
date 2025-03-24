2f4ec3c25e55fa61bfbdeddff4280f6839bc4558
samalatesta <samalate@bu.edu>
2025-03-24 13:14:46 -0400

    Improve documentation for internal functions.

--- R/utils.R
+++ R/utils.R
@@ -1,105 +1,103 @@
-#' Propose simultaneous events
+#' Propose simultaneous clinical states
+#' @description
+#' Generate a simulataneous disease sequence given a sequence of clinical states.
 #'
 #' @param info A data frame.
-#' @param all_seqs List of all possible sequences.
-#' @return A ggplot object.
+#' @param all_seqs Data frame of all possible disease sequences.
+#' @return A data frame.
 #' @keywords internal
 #'
 
-
 get_group <- function( info=data.frame(), all_seqs){
-  #D=make_D(N)
-  #seq=get_seq(D)
-  #info<- seq
-
+  #arrange sequence of states
   info <- info %>% dplyr::arrange(order)
   N=dim(info)[1]
-  #all_seqs=add
 
+  #select disease sequence allowing multiple states per disease class
+  #code allows for multiple states in one measure to occur at the same class
+  #uncomment code below if clinical measures should be unique per class
   all_seqs2=all_seqs
   all_seqs=data.frame(t(apply(all_seqs[,1:N], 1, function(i) paste(i, info$bio) )))
   all_seqs$unique= apply(all_seqs[,1:N], 1, function(x) length(unique(x)))==N
 
-  final <-  cbind(all_seqs2, data.frame(unique=all_seqs$unique)) %>% dplyr::filter(unique==T)
+  #all possible seqs after criteria above
+  final <-  cbind(all_seqs2, data.frame(unique=all_seqs$unique))#%>% dplyr::filter(unique==T)
+  #randomly select one sequence to use
   select <- sample(1:dim(final)[1],1)
 
+  #add to info df
   info$sub<- as.numeric(final[select,1:N])
 
-
-  #print(info)
+  # print(info)
   return(info)
 }
 
-#measure_info <- data.frame(clinical_measure=c("measure1", "measure1", "measure2", "measure2", "measure2", "measure3"),event_number = c(1,2,1,2,3,1), event_name=c(c("X1", "X2", "X3", "X4", "X5", "X6")))
 
-
-#' Propose sequence as start point for maximum likelihood estimation.
+#' Sequence initialization
+#' @description
+#' Generate initial sequence as start point for maximum likelihood search.
 #'
-#' @param info A data frame.
+#' @param info A data frame containing the clinical measures and corresponding clinical
+#' states to generate a sequence from.
 #' @return A data frame.
 #' @keywords internal
 #'
-#### Initialize sequence events
+
 get_seq <- function(info=data.frame()){
+  #save info df to work with
+  bio_info_long <- info
+  #update col names
+  colnames(bio_info_long) <- c("bio", "event", "var_name")
 
-  #transform D to long form
-  bio_info_long<- data.frame(bio=NA, event=NA)
-  for(i in 1: dim(info)[1]){
-    add <- data.frame(bio=rep(info[i,1]), event = c(1:info[i,2]))
-    bio_info_long <- rbind(bio_info_long, add)
-
-  }
-
-  bio_info_long <- bio_info_long %>% dplyr::filter(is.na(event)==F)
-
-  #bio_info_long <- ml[[2]][[1]]
   N=dim(bio_info_long)[1]
+  #save original order of states
   bio_info_long$pos = c(1:N)
 
+  #sample from clinical measures w/o replacement as new order and save as temp df
+  temp<-sample(bio_info_long$bio, N)
 
-  all_perms <- combinat::permn(bio_info_long$bio)
-  r <- sample(1:length(all_perms),1)
-  temp <- all_perms[[r]]
-
-
+  #update order in df
   bio_info_long_temp <- bio_info_long
   bio_info_long_temp$temp <- temp
-  bio_info_long_temp <- bio_info_long_temp %>% dplyr::arrange(event,temp)
+  bio_info_long_temp <- bio_info_long_temp %>% dplyr::arrange(temp)
   bio_info_long_temp$order <- c(1:dim(bio_info_long_temp)[1])
+  #bio_info_long_temp
+  bio_info_long_new <- bio_info_long #%>% dplyr::arrange(pos) %>% dplyr::select(-temp)
+  bio_info_long_new$order <- bio_info_long_temp$pos
 
-  bio_info_long_new <- bio_info_long_temp %>% dplyr::arrange(pos) %>% dplyr::select(-temp)
-  #bio_info_long_new
   return(bio_info_long_new)
-
 }
 
 
 
 
-#' Generate data frame of all possible simultaneous sequences.
+
+#' All possible sequences
 #'
-#' @param N Number of events.
+#' @description
+#' Generate data frame of all possible simultaneous sequences for N clinical states.
+#'
+#' @param N Number of clinical states.
 #' @return A data frame.
 #' @keywords internal
 #'
 #
 
-
 possible_seqs <- function(N=numeric()){
-
+  #filter such that first disease class is 1 and are monotonically non-decreasing
   filter_rows <- function(g, x) {
     ok  <- function(z) all(diff(z) %in% 0:1)
     out <- g[apply(g, 1, ok), ]
     replace(out, TRUE, lapply(out, \(i) x[i]))
   }
+  #generate all seqs
   f2 <- function(x = c(1:N), n=N+1, n1=2) {
     data.frame(as.list(rep(1, n1)),
                gtools::combinations(length(x), n-n1, repeats.allowed = TRUE)) |>
       filter_rows(x)
   }
-
-  # test runs
-  all=f2() # as per question
+  #generate all possible sequences of size N and filter using filter_rows and f2 functions
+  all=f2()
   all <- all[,-1]
   return(all)
 }
