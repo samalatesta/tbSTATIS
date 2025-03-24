a407af929cb2a905794eebf10384d2b2161916db
samalatesta <samalate@bu.edu>
2025-03-24 14:03:29 -0400

    Update bootstrap resampling and doc

--- R/bootstrap_seq.R
+++ R/bootstrap_seq.R
@@ -1,13 +1,12 @@
-#' Find maximum likelihood estimate
+#' Quantify sequence uncertainty with bootstrap resampling
 #'
-#' @param data A data frame.
-#' @param clinical_info A data frame
-#' @param p_vec Unique row identifier.
-#' @param nstart Number of initialization points
-#' @param initial_iter Number of iterations
-#' @param z number of bootstrap samples
-#' @return A data frame
-
+#' @param data A data frame of observed data.
+#' @param clinical_info A data frame with clinical measures and clinical states.
+#' @param p_vec A vector of clinical measure accuracies to fit TB-STATIS.
+#' @param nstart Number of initialized sequences for each run of TB-STATIS.
+#' @param initial_iter Number of iterations for TB-STATIS.
+#' @param z Number of bootstrap resamples to perform.
+#' @return A data frame of sequences estimated for bootstrap resamples.
 
 
 bootstrap_seq <- function(z, data,clinical_info, p_vec, nstart, initial_iter ){
